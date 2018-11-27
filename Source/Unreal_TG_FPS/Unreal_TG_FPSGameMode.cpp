b0080213c15361311606b872db5bf48f55f5e271
William Wylie-Modro <williamwyliemodro@gmail.com>
2018-11-26 16:23:18 -0800

    Guards now have Gun actor, but introducted bug where they are no longer patrolling

--- Source/Unreal_TG_FPS/Unreal_TG_FPSGameMode.cpp
+++ Source/Unreal_TG_FPS/Unreal_TG_FPSGameMode.cpp
@@ -9,7 +9,7 @@ AUnreal_TG_FPSGameMode::AUnreal_TG_FPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
--- Source/Unreal_TG_FPS/Unreal_TG_FPSHUD.cpp
+++ Source/Unreal_TG_FPS/Unreal_TG_FPSHUD.cpp
@@ -10,7 +10,7 @@
 AUnreal_TG_FPSHUD::AUnreal_TG_FPSHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
