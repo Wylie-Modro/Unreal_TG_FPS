ef4fe0e4cd7abb6dbac6d1f3b3a1731b54c1a5db
William Wylie-Modro <williamwyliemodro@gmail.com>
2018-12-01 13:12:03 -0800

    Fixed Guard movement bug. Improved Guard annimations. Make Guards shoot on aim.

--- Source/Unreal_TG_FPS/Weapons/Gun.h
+++ Source/Unreal_TG_FPS/Weapons/Gun.h
@@ -34,6 +34,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	/** Fires a projectile. */
+	UFUNCTION(BlueprintCallable, Category = "Fire")
 	void OnFire();
 
 	/** Projectile class to spawn */
