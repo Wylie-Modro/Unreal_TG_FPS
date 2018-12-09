f88535edb17d7902d05ed39a4a20d171adbf7046
William Wylie-Modro <williamwyliemodro@gmail.com>
2018-12-08 16:39:06 -0800

    Converted Character_BP into C++, Refactored shooting logic

--- Source/Unreal_TG_FPS/Unreal_TG_FPSGameMode.cpp
+++ Source/Unreal_TG_FPS/Unreal_TG_FPSGameMode.cpp
@@ -9,7 +9,7 @@ AUnreal_TG_FPSGameMode::AUnreal_TG_FPSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Character_BP"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
